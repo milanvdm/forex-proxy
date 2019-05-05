fefbab92eab5ecddfc1467597311d6389e4f28bc
milanvdm <milanvdm@hotmail.com>
2019-05-05 13:29:28 +0900

    Add Internal error

--- core/src/main/scala/forex/http/rates/errors.scala
+++ core/src/main/scala/forex/http/rates/errors.scala
@@ -6,11 +6,13 @@ object errors {
 
   sealed trait Error extends Throwable
   object Error {
+    final case class Internal(reason: String) extends Error
     final case class ParseFailure(msg: String) extends Error
     final case class RateLookupFailed(msg: String) extends Error
   }
 
   def toApiError(error: RatesProgramError): Error = error match {
+    case RatesProgramError.Internal(reason) ⇒ Error.Internal(reason)
     case RatesProgramError.RateLookupFailed(msg) ⇒ Error.RateLookupFailed(msg)
   }
 }
--- core/src/main/scala/forex/programs/rates/errors.scala
+++ core/src/main/scala/forex/programs/rates/errors.scala
@@ -6,10 +6,12 @@ object errors {
 
   sealed trait Error extends Throwable
   object Error {
+    final case class Internal(reason: String) extends Error
     final case class RateLookupFailed(msg: String) extends Error
   }
 
   def toProgramError(error: RatesServiceError): Error = error match {
+    case RatesServiceError.Internal(reason) ⇒ Error.Internal(reason)
     case RatesServiceError.OneForgeLookupFailed(msg) ⇒ Error.RateLookupFailed(msg)
   }
 }
