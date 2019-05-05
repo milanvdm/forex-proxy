f3761f7e95b42f57c09b7b16bd2a7f104dc50665
milanvdm <milanvdm@hotmail.com>
2019-05-05 13:29:52 +0900

    Add happy OneForgeLive test

---
+++ core/src/test/scala/forex/services/rates/interpreters/live/OneForgeLiveSpec.scala
@@ -0,0 +1,84 @@
+package forex.services.rates.interpreters.live
+
+import cats.effect.IO
+import forex.config.OneForgeConfig
+import forex.domain.{ Currency, Pair, Price, Rate, Timestamp }
+import forex.services.RatesServices
+import io.circe.Json
+import io.circe.literal._
+import org.http4s.circe._
+import org.http4s.client.Client
+import org.http4s.dsl.Http4sDsl
+import org.http4s.implicits._
+import org.http4s.{ HttpRoutes, Response }
+import org.scalatest.{ FlatSpec, Matchers }
+
+class OneForgeLiveSpec extends FlatSpec with Matchers with Http4sDsl[IO] {
+  import OneForgeLiveSpec._
+
+  val config = OneForgeConfig("http://myservice", "secret")
+
+  private def mockHttpClient(response: IO[Response[IO]]): Client[IO] = {
+    object PairsQueryParam extends QueryParamDecoderMatcher[String]("pairs")
+    object ApiKeyQueryParam extends QueryParamDecoderMatcher[String]("api_key")
+
+    Client.fromHttpApp[IO] {
+      HttpRoutes
+        .of[IO] {
+          case GET → Root / "quotes" :? PairsQueryParam(_) +& ApiKeyQueryParam(_) ⇒
+            response
+        }
+        .orNotFound
+    }
+  }
+
+  "getAllRates" should "return all Rates if Forge API responds correctly" in {
+
+    val httpClient = mockHttpClient(Ok(successResponse))
+    val ratesService = RatesServices.live[IO](config, httpClient)
+
+    val expected =
+      Set(
+        Rate(
+          Pair(Currency.EUR, Currency.USD),
+          Price(1.12008),
+          Timestamp.fromEpoch(1556958556)
+        ),
+        Rate(
+          Pair(Currency.USD, Currency.JPY),
+          Price(111.093),
+          Timestamp.fromEpoch(1556958556)
+        )
+      )
+
+    val response = ratesService.getAllRates.unsafeRunSync()
+
+    response should contain theSameElementsAs expected
+
+  }
+
+}
+
+object OneForgeLiveSpec {
+
+  val successResponse: Json =
+    json"""
+          [
+            {
+              "symbol": "EURUSD",
+              "bid": 1.11997,
+              "ask": 1.1202,
+              "price": 1.12008,
+              "timestamp": 1556958556
+            },
+            {
+              "symbol": "USDJPY",
+              "bid": 111.083,
+              "ask": 111.103,
+              "price": 111.093,
+              "timestamp": 1556958556
+            }
+          ]
+        """
+
+}
