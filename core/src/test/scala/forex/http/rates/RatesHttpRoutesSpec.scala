bc384c5e0cdbcd4d4e2451d784971a591f19caba
milanvdm <milanvdm@hotmail.com>
2019-05-18 21:00:52 +0900

    Add pending RatesHttpRoutesSpec

---
+++ core/src/test/scala/forex/http/rates/RatesHttpRoutesSpec.scala
@@ -0,0 +1,11 @@
+package forex.http.rates
+
+import org.scalatest.{ FlatSpec, Matchers }
+
+class RatesHttpRoutesSpec extends FlatSpec with Matchers {
+
+  "routes" should "correctly decode from and to" in pending
+
+  it should "correctly encode the rate response" in pending
+
+}
